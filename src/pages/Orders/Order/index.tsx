b6c6ebd8f02d342e9a5b1898222b79862d5c1419
Adriano Biolchi <adrianobiolchi@outlook.com>
2023-05-17 10:29:13 -0300

    alter layout

--- src/pages/Orders/Order/index.tsx
+++ src/pages/Orders/Order/index.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState, useRef } from 'react'
 
 import ReactToPrint from 'react-to-print'
 
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+
 import { useParams } from 'react-router-dom'
 
 import { api } from '../../../lib/axios'
@@ -30,21 +33,76 @@ const Label: React.FC<{ item: any }> = ({ item }) => (
   </div>
 )
 
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+interface Item {
+  item :{
+    id: string;
+    codigo: string;
+    descricao: string;
+    quantidade: string;
+  }
+}
+
+
+
 const LabelPrinting: React.FC = () => {
   const { id } = useParams()
-  const [items, setItems] = useState<any[]>([])
+  const [items, setItems] = useState<Item[]>([])
   const printRef = useRef(null)
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     const fetchItems = async () => {
-      const response = await api.get<any[]>(`/vendas/${id}`)
+      const response = await api.get<Item[]>(`/vendas/${id}`)
       const { itens } = response.data
       setItems(itens)
     }
 
     fetchItems()
   }, [id])
+ 
+  
+  const createPDF = () => {
+    let content = [];
+   
+  
+    for (let i = 0; i < items.length; i += 3) {
+      const firstItem = items[i] ? `${items[i].item.codigo}\n${items[i].item.descricao}\n${items[i].item.quantidade}` : '';
+      const secondItem = items[i+1] ? `${items[i+1].item.codigo}\n${items[i+1].item.descricao}\n${items[i+1].item.quantidade}` : '';
+      const thirdItem = items[i+2] ? `${items[i+2].item.codigo}\n${items[i+2].item.descricao}\n${items[i+2].item.quantidade}` : '';
+      
+      content.push({
+        table: {
+          widths: ['*', '*', '*'],
+          body: [
+            [
+              { text: firstItem, style: 'labelStyle' },
+              { text: secondItem, style: 'labelStyle' },
+              { text: thirdItem, style: 'labelStyle' }
+            ],
+          ]
+        },
+        layout: 'lightHorizontalLines',
+        margin: [0, 5, 0, 5]
+      });
+    }
+
+    const docDefinition = {
+      pageSize: { width: 100, height: 'auto' },
+      pageMargins: [ 5, 5, 5, 5 ],
+      content,
+      styles: {
+        labelStyle: {
+          fontSize: 8,
+          alignment: 'justify',
+          margin: [5, 5, 5, 5],
+        },
+      },
+    };
+  
+    pdfMake.createPdf(docDefinition).open();
+  };
 
   return (
     <OrderContainer>
@@ -66,7 +124,7 @@ const LabelPrinting: React.FC = () => {
               <th>Descrição</th>
               <th>Quantidade</th>
               <th>
-                <ReactToPrint 
+                {/* <ReactToPrint 
                   trigger={() =>
                     isLoading === true ? (
                       <PrintButton>Gerando...</PrintButton>
@@ -75,7 +133,8 @@ const LabelPrinting: React.FC = () => {
                     )
                   }
                   content={() => printRef.current}
-                />
+                /> */}
+                 <PrintButton onClick={createPDF}>Imprimir etiquetas</PrintButton>
               </th>
             </tr>
           </thead>
